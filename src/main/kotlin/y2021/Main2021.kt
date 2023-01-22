c648c5de594cae922048df6b4b8b92f6e1d67b4e
Sam <samdc@apadmi.com>
2023-01-22 15:50:13 +0000

    tweak setup

--- src/main/kotlin/y2021/Main2021.kt
+++ src/main/kotlin/y2021/Main2021.kt
@@ -5,11 +5,11 @@ import utils.runTimed
 
 object Main2021 {
     private val allDays =
-        (1..25).map { Class.forName("y2021.day$it.Main") }
+        (1..25).take(1).map { Class.forName("y2021.day$it.Main") }
 
     fun runAll() = allDays.forEachIndexed { index, puzzle ->
         println(
-            "Day ${(index + 1).debug()} Sample: ${
+            "Day ${(index + 1).debug()} Part 1 - Sample: ${
                 runTimed {
                     (puzzle.getConstructor().newInstance() as Puzzle).runPart1(
                         parseFile(
@@ -22,7 +22,33 @@ object Main2021 {
             }"
         )
         println(
-            "Day ${(index + 1).debug()}   Real: ${
+            "Day ${(index + 1).debug()} Part 1  -  Real: ${
+                runTimed {
+                    (puzzle.getConstructor().newInstance() as Puzzle).runPart1(
+                        parseFile(
+                            2021,
+                            index + 1,
+                            "data.txt"
+                        ), RunMode.Real
+                    )
+                }
+            }"
+        )
+        println(
+            "Day ${(index + 1).debug()} Part 2 - Sample: ${
+                runTimed {
+                    (puzzle.getConstructor().newInstance() as Puzzle).runPart2(
+                        parseFile(
+                            2021,
+                            index + 1,
+                            "sample.txt"
+                        ), RunMode.Sample
+                    )
+                }
+            }"
+        )
+        println(
+            "Day ${(index + 1).debug()} Part 2  -  Real: ${
                 runTimed {
                     (puzzle.getConstructor().newInstance() as Puzzle).runPart2(
                         parseFile(
--- src/main/kotlin/y2021/day1/Main.kt
+++ src/main/kotlin/y2021/day1/Main.kt
@@ -4,6 +4,11 @@ import utils.Puzzle
 import utils.RunMode
 
 class Main: Puzzle {
-    override fun runPart1(data: List<String>, runMode: RunMode): Any = ""
+    override fun runPart1(data: List<String>, runMode: RunMode) = data.map { it.toInt() }.let { measurments ->
+        (1..measurments.lastIndex).count { index ->
+            measurments[index] > measurments[index - 1]
+        }
+    }
+
     override fun runPart2(data: List<String>, runMode: RunMode): Any = ""
 }
