ad585fde0d1006807c19f21e294764d5752bdde8
Sam <samdc@apadmi.com>
2023-01-22 22:12:56 +0000

    possibly working?

--- src/main/kotlin/y2021/day6/Main.kt
+++ src/main/kotlin/y2021/day6/Main.kt
@@ -2,10 +2,35 @@ package y2021.day6
 
 import utils.Puzzle
 import utils.RunMode
+import utils.debug
 
-class Main: Puzzle {
+class Main : Puzzle {
 
-    private fun runSimulation(days: Int, data: List<String>) = data.first().split(",").map { it.toLong() }.let { values ->
+    var totalFish: Long = 0L
+    private fun runSimulation(totalDays: Int, fishes: List<Int>): Long {
+        fishes.forEach { dayOfFirstChild ->
+            runIndividualSimulation(dayOfFirstChild, totalDays)
+        }
+
+        totalFish += fishes.size
+        return totalFish
+    }
+
+    private fun runIndividualSimulation(dayOfFirstChild: Int, totalDays: Int) {
+        if (dayOfFirstChild >= totalDays) {
+            return
+        }
+
+        val childrenProduced = 1 + (totalDays - dayOfFirstChild - 1) / 7
+//        println("Fish which starts at ${dayOfFirstChild.debug()} produced $childrenProduced")
+        totalFish += childrenProduced
+
+        repeat(childrenProduced) { childIndex ->
+            runIndividualSimulation(9 + dayOfFirstChild + 7 * childIndex, totalDays)
+        }
+    }
+
+    private fun runNaiive(days: Int, values: List<Long>): Any {
         val currentState = values.toMutableList()
 
         repeat(days) {
@@ -20,13 +45,16 @@ class Main: Puzzle {
             }
 
             (0 until numNew).map { 8L }.forEach { currentState.add(it) }
-
-            println("Day ${it + 1} ${currentState.size}")
+//            println("After day ${it+1}  $currentState")
         }
 
-        currentState.count()
+        return currentState.count()
+
     }
 
-    override fun runPart1(data: List<String>, runMode: RunMode) = runSimulation(80, data)
-    override fun runPart2(data: List<String>, runMode: RunMode) = runSimulation(256, data)
+    override fun runPart1(data: List<String>, runMode: RunMode) =
+        runSimulation(80, data.first().split(",").map { it.toInt() })
+
+    override fun runPart2(data: List<String>, runMode: RunMode) =
+        runSimulation(256, data.first().split(",").map { it.toInt() })
 }
