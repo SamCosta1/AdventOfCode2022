83b20bdcf11a3a61d88cfa4f0d282f6ea1cf3d2d
Sam <samdc@apami.com>
2024-12-06 16:13:33 +0000

    part2 not working

--- src/main/kotlin/y2024/day6/Main.kt
+++ src/main/kotlin/y2024/day6/Main.kt
@@ -1,20 +1,21 @@
 package y2024.day6
 
 import puzzlerunners.Puzzle
-import puzzlerunners.NotStarted
+import utils.GenericGrid
 import utils.MovementDirection
 import utils.Point
 import utils.RunMode
 
 class Main(
     override val part1ExpectedAnswerForSample: Any = 41,
-    override val part2ExpectedAnswerForSample: Any = NotStarted,
+    override val part2ExpectedAnswerForSample: Any = 6,
     override val isComplete: Boolean = true
-): Puzzle {
+) : Puzzle {
     override fun runPart1(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (startPos, grid) ->
         var nextPos = startPos
         var direction = MovementDirection.North
         val points = mutableSetOf(startPos)
+        var iterations = 0
         while (grid[nextPos] != Parser.Item.Outside) {
             if (grid[nextPos(direction)] == Parser.Item.Obstacle) {
                 direction = direction.turnRight90Degrees
@@ -22,9 +23,86 @@ class Main(
                 nextPos = nextPos(direction)
                 points.add(nextPos)
             }
+            iterations++
         }
 
+        println("Iterations $iterations")
+        points.forEach { grid[it] = Parser.Item.Outside }
         return@let points.size - 1
     }
-    override fun runPart2(data: List<String>, runMode: RunMode): Any = ""
+
+    // 1988 1998 is too high
+    fun GenericGrid<Parser.Item>.causesLoop(
+        startPos: Point,
+        startDirection: MovementDirection
+    ): Boolean {
+        var thisPos = startPos
+        var direction = startDirection
+        val newPointsAndDirections = mutableMapOf<Point, MutableSet<MovementDirection>>()
+        while (this[thisPos] != Parser.Item.Outside) {
+            if (this[thisPos(direction)] == Parser.Item.Obstacle) {
+                direction = direction.turnRight90Degrees
+                continue
+            }
+
+            if (this[thisPos] == Parser.Item.Outside) {
+                return false
+            } else if (
+                    newPointsAndDirections[thisPos]?.contains(direction) == true
+            ) {
+                return true
+            } else {
+                newPointsAndDirections.getOrPut(thisPos) { mutableSetOf() }.add(direction)
+                thisPos = thisPos(direction)
+            }
+        }
+
+        return false
+    }
+
+    override fun runPart2(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (startPos, grid) ->
+        var thisPos = startPos
+        var direction = MovementDirection.North
+
+        val solutions = mutableListOf<Pair<Point, MovementDirection>>()
+        var iterations = 0
+        while (grid[thisPos] != Parser.Item.Outside) {
+            if (grid[thisPos(direction)] == Parser.Item.Obstacle) {
+                direction = direction.turnRight90Degrees
+                continue;
+            }
+
+            val nextPos = thisPos(direction)
+            if (grid[nextPos] == Parser.Item.Outside) {
+                break;
+            }
+            grid[nextPos] = Parser.Item.Obstacle
+
+            if (
+                grid.causesLoop(
+                    thisPos,
+                    direction.turnRight90Degrees,
+                )
+            ) {
+                solutions.add(nextPos to direction.turnRight90Degrees)
+            }
+            grid[nextPos] = Parser.Item.Free
+
+            thisPos = nextPos
+
+            iterations++
+//            if (iterations % 10 == 0) {
+//            println("Iterated $iterations / 5705")
+//            }
+        }
+
+        solutions.removeIf { it.first == startPos }
+        solutions.forEach {
+            grid[it.first] = Parser.Item.Outside
+        }
+//        println("solutions $solutions")
+        println(grid)
+        return@let solutions.map { it.first }.toSet().size
+    }
+
 }
