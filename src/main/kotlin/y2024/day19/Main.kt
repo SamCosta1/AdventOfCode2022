3fa9914031cf2ae2ead7194a79f14a0f47099987
Sam <samdc@apami.com>
2024-12-21 13:54:52 +0000

    day19 part1

--- src/main/kotlin/y2024/day19/Main.kt
+++ src/main/kotlin/y2024/day19/Main.kt
@@ -6,55 +6,44 @@ import utils.RunMode
 
 class Main(
     override val part1ExpectedAnswerForSample: Any = 6,
-    override val part2ExpectedAnswerForSample: Any = 16,
+    override val part2ExpectedAnswerForSample: Any = NotStarted,
     override val isComplete: Boolean = true
 ) : Puzzle {
     override fun runPart1(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (towels, combos) ->
         cache.clear()
-        println("$runMode ${towels.size}")
+
+        println("no towels ${towels.size}")
+        combos.forEach { combo ->
+            println(towels.filter { combo.contains(it) }.size)
+        }
         combos.count { combo ->
-            val number = numberOfSols(combo, towels).size
-            println("Combo $combo | $number")
+            val number = numberOfSols(combo, towels)
+            println("Number fo sols $combo $number")
             number > 0
         }.also { println(it) }
     }
 
-    val cache = mutableMapOf<String, List<Set<String>>>()
+    val cache = mutableMapOf<String, Long>()
 
-    // 208 too low
-    private fun numberOfSols(combo: String, towels: Set<String>): List<Set<String>> {
+    private fun numberOfSols(combo: String, towels: Set<String>): Long {
         if (cache[combo] != null) {
             return cache[combo]!!
         }
 
         if (combo.length <= 1 && !towels.contains(combo)) {
-            return emptyList()
+            return 0
         }
-        var count = mutableListOf<Set<String>>()
-        if (towels.contains(combo)) {
-            count += setOf(towels)
-        }
+        var count = if (towels.contains(combo)) 1L else 0L
         combo.dropLast(1).forEachIndexed { index, c ->
             val firstHalf = numberOfSols(combo.take(index + 1), towels)
-            val secondHalf = numberOfSols(combo.drop(index + 1), towels)
-            if (firstHalf.isNotEmpty() && secondHalf.isNotEmpty()) {
-                firstHalf.forEach { first ->
-                    secondHalf.forEach { second ->
-                        count += first.union(second)
-                    }
-                }
+            if (firstHalf > 0L) {
+                val secondHalf = numberOfSols(combo.drop(index + 1), towels)
+                 count += firstHalf.coerceAtMost(1) * secondHalf.coerceAtMost(1)
             }
         }
         cache[combo] = count
         return count
     }
 
-    override fun runPart2(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (towels, combos) ->
-        cache.clear()
-        combos.sumOf { combo ->
-            val number = numberOfSols(combo, towels).size
-            println("Combo $combo | $number")
-            number
-        }.also { println(it) }
-    }
+    override fun runPart2(data: List<String>, runMode: RunMode): Any = ""
 }
