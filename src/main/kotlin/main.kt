81538fc3f0cfaf9b49ccd28dd057ede68ad869e5
Sam <samdc@apami.com>
2024-12-22 16:17:42 +0000

    day 19

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,6 +13,6 @@ fun main() {
 //    println(AsciiTableGenerator.formatInProgress(Main2024.runAll()))
 //    println(AsciiGraphGenerator.formatGraph(Main2024.runAll()))
 
-//    println(AsciiTableGenerator.formatInProgress(Main2023.runLatest()))
-    println(AsciiTableGenerator.formatInProgress(Main2024.runLatest()))
+    println(AsciiTableGenerator.formatInProgress(Main2024.run(19)))
+//    println(AsciiTableGenerator.formatInProgress(Main2024.runLatest()))
 }
--- src/main/kotlin/y2024/day19/Main.kt
+++ src/main/kotlin/y2024/day19/Main.kt
@@ -1,83 +1,67 @@
 package y2024.day19
 
-import puzzlerunners.Puzzle
 import puzzlerunners.NotStarted
+import puzzlerunners.Puzzle
 import utils.RunMode
-import kotlin.math.pow
+
 
 typealias Combo = Pair<ULong, ULong>
 
 class Main(
     override val part1ExpectedAnswerForSample: Any = 6,
-    override val part2ExpectedAnswerForSample: Any = NotStarted,
+    override val part2ExpectedAnswerForSample: Any = 16L,
     override val isComplete: Boolean = true
 ) : Puzzle {
     override fun runPart1(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (towels, combos) ->
-        println("no towels ${towels.size}")
-        combos.maxOf { combo ->
-            towels.filter { combo.contains(it) }.size.also {
-//            println("$combo $it")
+        val trie = TrieNode(false)
+        towels.forEach { trie.insert(it.reversed()) }
 
-            }
-        }.also { println("max  $it") }
-        val bits = 63
-        val base = 2.0.pow(bits).toULong()
-
-        println("base ${base.toString(2)} ${base.toString(2).length}")
-        println("0    ${(base.shr(0)).toString(2)}")
-        println("1    0${(base.shr(1)).toString(2)}")
-        println("2    00${(base.shr(2)).toString(2)}")
-        println("3    000${(base.shr(3)).toString(2)}")
-        println("64   000${(base.shr(63)).toString(2)}")
-
-        var total = 0L
         combos.count { combo ->
-            cache.clear()
-            val subTowels = towels.filter { combo.contains(it) }
-            val subTowelsMap = subTowels.associateWith {
-                val index = subTowels.indexOf(it)
-                if (index <= 63) {
-                    Pair(base.shr(index), 0UL)
-                } else {
-                    Pair(0UL, base.shr(index - 63))
-                }
-            }
-            val number = numberOfSols(combo, subTowelsMap)?.size ?: 0
-            total += number
-            println("Number fo sols $combo $number")
-            number > 0
+            numberOfCombos(combo, trie) > 0
         }
-            .also {
-                println("$runMode $it $total")
-            }
     }
 
-    val cache = mutableMapOf<String, Set<Combo>>()
+    class TrieNode(var isEndOfWord: Boolean, val children: Array<TrieNode?> = arrayOfNulls(26)) {
+        fun insert(string: String) {
+            var current = this
 
-    private fun numberOfSols(combo: String, towels: Map<String, Combo>): Set<Combo>? {
-        if (cache[combo] != null) {
-            return cache[combo]
-        }
+            string.forEachIndexed { index, char ->
+                val childNodeIndex = char - 'a'
 
-        if (combo.length <= 1 && !towels.contains(combo)) {
-            return null
+                if (current.children[childNodeIndex] == null) {
+                    current.children[childNodeIndex] = TrieNode(false)
+                }
+                current = current.children[childNodeIndex]!!
+            }
+            current.isEndOfWord = true
         }
-        val count = if (towels.contains(combo)) mutableSetOf(towels[combo]!!) else mutableSetOf<Combo>()
-        combo.dropLast(1).forEachIndexed { index, c ->
-            val firstHalf = numberOfSols(combo.take(index + 1), towels)
-            if (!firstHalf.isNullOrEmpty()) {
-                val secondHalf = numberOfSols(combo.drop(index + 1), towels)
-                firstHalf.forEach { c1 ->
-                    secondHalf?.forEach { c2 ->
-                        count.add(Pair(c1.first xor c2.first, c1.second xor c2.second))
-                    }
+    }
+    override fun runPart2(data: List<String>, runMode: RunMode) = Parser.parse(data).let { (towels, combos) ->
+        val trie = TrieNode(false)
+        towels.forEach { trie.insert(it.reversed()) }
+
+        combos.sumOf { combo ->
+            numberOfCombos(combo, trie)
+        }
+    }
+
+    private fun numberOfCombos(combo: String, trie: TrieNode): Long {
+        val count = Array<Long>(combo.length) { 0 }
+
+        for (i in count.indices) {
+            var ptr: TrieNode? = trie
+            for (j in i downTo 0) {
+                val ch: Char = combo[j]
+                val index = ch.code - 'a'.code
+                if (ptr?.children?.get(index) == null) {
+                    break
+                }
+                ptr = ptr.children[index]
+                if (ptr!!.isEndOfWord) {
+                    count[i] = count[i] + if (j > 0) count[j - 1] else 1
                 }
             }
         }
-        println(count.size)
-        cache[combo] = count
-        return count
+        return count[count.lastIndex]
     }
-
-    override fun runPart2(data: List<String>, runMode: RunMode): Any = ""
 }
