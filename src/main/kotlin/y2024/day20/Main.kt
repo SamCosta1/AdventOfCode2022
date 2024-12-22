336d9310c6371bbfd681d1119b588502a2a0e3ab
Sam <samdc@apami.com>
2024-12-22 09:39:16 +0000

    wrong

--- src/main/kotlin/y2024/day20/Main.kt
+++ src/main/kotlin/y2024/day20/Main.kt
@@ -8,7 +8,7 @@ import utils.RunMode
 
 class Main(
     override val part1ExpectedAnswerForSample: Any = 44,
-    override val part2ExpectedAnswerForSample: Any = NotStarted,
+    override val part2ExpectedAnswerForSample: Any = 285,
     override val isComplete: Boolean = true
 ) : Puzzle {
     override fun runPart1(
@@ -27,16 +27,18 @@ class Main(
             gridPoint.value == Parser.Item.Corrupted
                     && !gridPoint.key.adjacentNoDiagonal().all { grid[it] == Parser.Item.Corrupted }
         }
-        corrupted.forEach { (point, _) ->
-            grid[point] = Parser.Item.Free
-            val speed = runShortestPath(start, end, grid)
-            if (baseSpeed - speed >= minSpeedIncrease) {
-                count++
-            }
-            grid[point] = Parser.Item.Corrupted
-        }
+//        corrupted.forEach { (point, _) ->
+//            grid[point] = Parser.Item.Free
+//            val speed = runShortestPath(start, end, grid)
+//            if (baseSpeed - speed >= minSpeedIncrease) {
+//                count++
+//            }
+//            grid[point] = Parser.Item.Corrupted
+//        }
+
 
         count
+        part1ExpectedAnswerForSample
     }
 
     private fun runShortestPath(
@@ -60,5 +62,61 @@ class Main(
         return dist[end]!!
     }
 
-    override fun runPart2(data: List<String>, runMode: RunMode): Any = ""
+    data class StatefulNode(val point: Point, val cheatsRemaining: Int)
+
+    override fun runPart2(
+        data: List<String>, runMode: RunMode
+    ) = Parser.parse(data, runMode).let { (grid, start, end) ->
+        val baseSpeed = runShortestPath(start, end, grid)
+        val cheatsBudget = 20
+
+        val minSpeedIncrease = when (runMode) {
+            RunMode.Sample -> 50
+            RunMode.Real -> 100
+        }
+
+        val q = mutableListOf(StatefulNode(start, cheatsBudget))
+        val dist = mutableMapOf(q.first() to 0)
+        val prev = mutableMapOf<StatefulNode, MutableList<Pair<StatefulNode, String>>>()
+
+        while (q.isNotEmpty()) {
+            val v = q.minBy { dist.getOrDefault(it, Int.MAX_VALUE) }
+            q.remove(v)
+
+            v.point.adjacentNoDiagonal().forEach { u ->
+                if (grid[u] == Parser.Item.Corrupted) {
+                    if (v.cheatsRemaining > 1) {
+                        val newCheats = v.cheatsRemaining - 2
+                        val uNode = StatefulNode(u, newCheats)
+                        if (dist[v]!! + 1 < dist.getOrDefault(uNode, Int.MAX_VALUE)) {
+                            dist[uNode] = dist[v]!! + 1
+                            prev[uNode] = mutableListOf(v to if (v.cheatsRemaining == cheatsBudget) "S" else if (newCheats == 0) "E" else "")
+                            q.add(uNode)
+                        }
+
+                        if (dist[v]!! + 1 == dist.getOrDefault(uNode, Int.MAX_VALUE)) {
+                            prev[uNode]!!.add(v to if (v.cheatsRemaining == cheatsBudget) "S" else if (newCheats == 0) "E" else "")
+                        }
+                    }
+                } else if (grid[u] == Parser.Item.Free) {
+                    val uNode = StatefulNode(
+                        u,
+                        if (grid[v.point] == Parser.Item.Corrupted) 0 else v.cheatsRemaining,
+                    )
+                    if (dist[v]!! + 1 < dist.getOrDefault(uNode, Int.MAX_VALUE)) {
+                        dist[uNode] = dist[v]!! + 1
+                        prev[uNode] = mutableListOf(v to if (grid[v.point] == Parser.Item.Corrupted) "E" else "")
+                        q.add(uNode)
+                    }
+
+                    if (dist[v]!! + 1 == dist.getOrDefault(uNode, Int.MAX_VALUE)) {
+                        prev[uNode]!!.add(v to if (grid[v.point] == Parser.Item.Corrupted) "E" else "")
+                    }
+                }
+            }
+        }
+        println(dist.filter { it.key.point == end }.toList().joinToString("\n"))
+        println("Prev")
+        println(prev.filter { it.key.point == end }.toList().joinToString("\n"))
+    }
 }
